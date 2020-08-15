6c2da965b7aab42cca466efe7be1f2087148687e
unknown <arouser2112@gmail.com>
2020-08-15 08:20:03 -0500

    simple imports and for bootstrap and react router dom, and text/styling for the start of my home page.

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar'
 import './App.css';
 
 class App extends React.Component {
@@ -9,15 +12,36 @@ class App extends React.Component {
       title: 'Avery Rouser',
       headerLinks: [
         {title: 'Home', path: '/'},
-        {title: 'Home', path: '/'},
-        {title: 'Home', path: '/'},
-      ]
+        {title: 'About', path: '/about'},
+        {title: 'Contact', path: '/contact'},
+      ],
+      home: {
+        title: "Be relentless",
+        subTitle: "Projects that make a difference",
+        subTitleText: "Check out my projects below"
+      },
+      About: {
+        title: "About Me",
+      },
+      Contact: {
+        title: "Let's Connect!",
+      }
     }
   }
 
   render() {
     return (
-      <div>Hello from React</div>
+        <Router>
+          <Container className="p-0" fluid={true}>
+            
+            <Navbar>
+              <Navbar.Brand>Avery Rouser</Navbar.Brand>
+            </Navbar>
+
+
+
+          </Container>
+        </Router>
       );
     }
   }
