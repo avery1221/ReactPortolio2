862af695798d8e06a75dfe6349d8fbebd3747abe
unknown <arouser2112@gmail.com>
2020-08-17 14:52:16 -0500

    created my pages as well as the routes to all of them.

--- portfolio2/src/App.js
+++ portfolio2/src/App.js
@@ -5,6 +5,9 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
 import Footer from './components/Footer';
+import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
+import ContactPage from './Pages/ContactPage';
 
 import './App.css';
 
@@ -50,6 +53,10 @@ class App extends React.Component {
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
+
+            <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+            <Route path="/about"  render={() => <AboutPage title={this.state.About.title} />}  />
+            <Route path="/contact"  render={() => <ContactPage title={this.state.Contact.title} />}  />
 
             <Footer />
 
